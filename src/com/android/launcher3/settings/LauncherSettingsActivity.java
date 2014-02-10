af948ab7bd8ff699f89914d36c4da0df023f4dca
htchoi1995 <htchoi1995@gmail.com>
2014-02-10 14:06:45 +0900

    Launcher3: implementation AboutActivity

--- src/com/android/launcher3/settings/LauncherSettingsActivity.java
+++ src/com/android/launcher3/settings/LauncherSettingsActivity.java
@@ -1,7 +1,15 @@
 package com.android.launcher3.settings;
 
+import android.os.Bundle;
 import android.preference.PreferenceActivity;
 
-public class LauncherSettingsActivity extends PreferenceActivity
-{
+import com.android.launcher3.R;
+
+public class LauncherSettingsActivity extends PreferenceActivity {
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.settings);
+	}
 }
