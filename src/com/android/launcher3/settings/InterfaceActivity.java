31141fe8f28bd7b358e1b0643a1987ed1b50b516
htchoi1995 <htchoi1995@gmail.com>
2014-03-01 23:42:08 +0900

    Launcher3: fix build warnings

--- src/com/android/launcher3/settings/InterfaceActivity.java
+++ src/com/android/launcher3/settings/InterfaceActivity.java
@@ -29,7 +29,7 @@ public class InterfaceActivity extends PreferenceActivity implements OnPreferenc
         try {
             mCurrent.setSummary(getPackageManager().getPackageInfo(iconPack, 0).applicationInfo.loadLabel(getPackageManager()));
         } catch (Exception e) {
-        	// ¿îÁö
+        	// Unji
         }
         
         // Show "home"
